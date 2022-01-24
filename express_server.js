cf0c3a9cb1a06d5a43900d430c5936f3e49881ba
Brendan Woo <woo.brendan@gmail.com>
2022-01-24 21:50:53 +0000

    created partials folder, containing _header file. modified index and show ejs files to include this header partial. fixed code in index to display the long url

--- express_server.js
+++ express_server.js
@@ -21,7 +21,7 @@ app.get('/urls', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
 const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateVars)
-});
+}); 
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
